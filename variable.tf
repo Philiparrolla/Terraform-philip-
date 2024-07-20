6d8005a5fe9d7c991527e3ff7bb65b5a930e90ef
Philip <philiparroll47@gmail.com>
2024-07-20 15:47:19 +0530

    tanuja

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = "passing to main.tf"
+    type = string
+    default = ""
+  
+}
+
+variable "instance_type" {
+    description = "passing to main.tf"
+    type = string
+    default = ""
+  
+}
+
+variable "key_name" {
+    description = "passing"
+    type = string
+    default = ""
+  
+}
